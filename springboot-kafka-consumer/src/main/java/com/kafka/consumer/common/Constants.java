d82cd432a68746b4bf974ac93bfe130963ff3fb1
Gi-Dragon <youn9354@naver.com>
2022-05-22 16:51:21 +0900

    consumer constants fix

--- springboot-kafka-consumer/src/main/java/com/kafka/consumer/common/Constants.java
+++ springboot-kafka-consumer/src/main/java/com/kafka/consumer/common/Constants.java
@@ -2,8 +2,8 @@ package com.kafka.consumer.common;
 
 public class Constants {
 
-    private final static String NAVER_SMS_SERVICE_ID = "ncp:sms:kr:12345678:test-sms";
+    private final static String NAVER_SMS_SERVICE_ID = "ncp:sms:kr:1234567890:test-sms";
     public final static String NAVER_SMS_URL = "/sms/v2/services/" + NAVER_SMS_SERVICE_ID + "/messages";
-    public final static String NAVER_SMS_FROM = "07012345678";
+    public final static String NAVER_SMS_FROM = "07032345678";
 
 }
