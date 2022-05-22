536533046a4ca46ce185ed4d0fbe4c101e68f638
Gi-Dragon <youn9354@naver.com>
2022-05-22 16:45:48 +0900

    test code 추가

--- springboot-kafka-consumer/src/test/java/com/kafka/consumer/service/SmsServiceTest.java
+++ springboot-kafka-consumer/src/test/java/com/kafka/consumer/service/SmsServiceTest.java
@@ -1,2 +1,26 @@
-package com.kafka.consumer.service;public class SmsService {
+package com.kafka.consumer.service;
+import com.kafka.consumer.domain.enums.ESmsType;
+import com.kafka.consumer.model.request.SmsRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(properties = "spring.profiles.active:dev")
+@Transactional
+class SmsServiceTest {
+
+    @Autowired
+    private SmsService smsService;
+
+    @Test
+    public void sms(){
+        SmsRequest.SmsRegular request = new SmsRequest.SmsRegular();
+        request.setUserTel("01092069357");
+        request.setSmsType(ESmsType.INSTALL);
+//        smsService.sendSms(request);
+    }
 }
