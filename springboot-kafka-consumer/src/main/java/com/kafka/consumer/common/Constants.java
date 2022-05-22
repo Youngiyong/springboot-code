6ec1f356f5a1f913c21149fd6d242acbc7145f26
Gi-Dragon <youn9354@naver.com>
2022-05-22 16:44:59 +0900

    consumer 정보 수정

--- springboot-kafka-consumer/src/main/java/com/kafka/consumer/common/Constants.java
+++ springboot-kafka-consumer/src/main/java/com/kafka/consumer/common/Constants.java
@@ -1,9 +1,9 @@
-package com.kafka.producer.domain.common;
+package com.kafka.consumer.common;
 
 public class Constants {
 
-    private final static String NAVER_SMS_SERVICE_ID = "ncp:sms:kr:262181829137:tsid_sms";
+    private final static String NAVER_SMS_SERVICE_ID = "ncp:sms:kr:12345678:test-sms";
     public final static String NAVER_SMS_URL = "/sms/v2/services/" + NAVER_SMS_SERVICE_ID + "/messages";
-    public final static String NAVER_SMS_FROM = "07043220043";
+    public final static String NAVER_SMS_FROM = "07012345678";
 
 }
--- springboot-kafka-consumer/src/main/java/com/kafka/consumer/domain/entity/snsHistory/SnsHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kafka.consumer.domain.entity.snsHistory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SnsHistoryRepository extends JpaRepository<SnsHistory, Long> {
-}
